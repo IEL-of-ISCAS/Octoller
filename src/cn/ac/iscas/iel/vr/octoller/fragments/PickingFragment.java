2b807b498abda95e7f6742edf2a70956fff5226a
Void Main <voidmain1313113@gmail.com>
2013-05-11 15:16:49 +0800

    change picking activity to picking fragment

---
+++ src/cn/ac/iscas/iel/vr/octoller/fragments/PickingFragment.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2013 Void Main Studio 
+ * Project:Octoller
+ * Author: voidmain
+ * Create Date: May 11, 20133:13:59 PM
+ */
+package cn.ac.iscas.iel.vr.octoller.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**   
+ * 
+ * 
+ * @Project Octoller
+ * @Package cn.ac.iscas.iel.vr.octoller.fragments
+ * @Class PickingFragment
+ * @Date May 11, 2013 3:13:59 PM
+ * @author voidmain
+ * @version 
+ * @since 
+ */
+public class PickingFragment extends Fragment {
+
+}
